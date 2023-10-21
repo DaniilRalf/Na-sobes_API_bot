5f8572d524dc3eca3d4ec4ff4490809fe1f7b1aa
DaniilRalf <ralf.danya@mail.ru>
2023-10-21 16:13:08 +0700

    start

---
+++ src/main/java/nasobesapi/NaSobesApiApplication.java
@@ -0,0 +1,13 @@
+package nasobesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaSobesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NaSobesApiApplication.class, args);
+    }
+
+}
